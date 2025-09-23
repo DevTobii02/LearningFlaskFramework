6b4205910b133fd96a7ef0401f15fd173c5ee5be
Adebayo Abdullahi <tobiithedev@gmail.com>
2025-09-24 00:26:06 +0100

    added python file app.py and added a function start flask  that renders a page index.html

---
+++ app.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def startflask():
+    # return 'Hello World!, I Am LearningFlaskFramework'
+    return  render_template("index.html") #renders the index.html page
+if __name__ == '__main__':
+    app.run(debug=True)
