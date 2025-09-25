e8eb151f771b23184b2a13312be5bf5c0fcaed37
Adebayo Abdullahi <tobiithedev@gmail.com>
2025-09-25 17:54:39 +0100

    added a dictionary to hold data for posts and its information

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+posts = [  #Dictionary to store post details in key and value pair
+    {
+        'author': 'Tobii Dev',
+        'title': 'My first post',
+        'content': 'My first post content',
+        'date_posted': 'April 5, 2018'
+    },
+    {
+        'author': 'Tobi Dev',
+        'title': 'My first post 1',
+        'content': 'My first post content 1',
+        'date_posted': 'April 5, 2020'
+    }
+]
 @app.route('/')
 def startflask():
     # return 'Hello World!, I Am LearningFlaskFramework'
-    return  render_template("index.html") #renders the index.html page
+    # Changed 'post=posts' to 'posts=posts' to match the variable used in the template
+    return  render_template("index.html", posts=posts) #renders the index.html page
 
 @app.route("/about")
 def aboutpage():
